3576608e83644a6690e454a60754cc30100ed7bb
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-01-04 15:44:59 +0200

    Added Data helper file

---
+++ app/code/local/Reea/Exacc/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Data
+ *
+ * @author pbc
+ */
+class Reea_Exacc_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    //put your code here
+}
+
